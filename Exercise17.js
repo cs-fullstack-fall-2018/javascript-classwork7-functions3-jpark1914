d3620948e0560bd6a7d44ba3319ebd728b6b2367
Jordan Parker <parkerj4321@gmail.com>
2018-07-31 14:24:12 -0500

    Exercise 17

---
+++ Exercise17.js
@@ -0,0 +1,14 @@
+function main() {
+    var int1 = 123;
+    var int2 = 213;
+    var int3 = 321;
+    sumFunc(int1, int2, int3);
+
+
+}
+function sumFunc(num1, num2, num3){
+    alert(parseInt(num1 + num2 + num3));
+}
+
+
+main();
---
+++ Exercise18.js
@@ -0,0 +1,11 @@
+function main() {
+
+    var num1 = 34;
+    var num2 = 345;
+
+    product(num1, num2);
+    
+
+}
+
+main();
