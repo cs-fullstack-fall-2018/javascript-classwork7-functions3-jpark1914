4ba3267cc80944d7f5f8b260efd49e571614fa16
Jordan Parker <parkerj4321@gmail.com>
2018-07-31 14:33:17 -0500

    The rest of the exercises

--- Exercise18.js
+++ Exercise18.js
@@ -3,9 +3,17 @@ function main() {
     var num1 = 34;
     var num2 = 345;
 
-    product(num1, num2);
-    
+    alert("The product of the numbers is: " + product(num1, num2));
+    alert("The quotient of the numbers is : " + quotient(num1, num2));
 
 }
+function product(number1, number2){
+    var multiply = number1 * number2;
+    return multiply;
+}
 
+function quotient(number1, number2){
+    var divide = number1/number2;
+    return divide;
+}
 main();
--- Exercise19.js
+++ Exercise19.js
@@ -1,5 +1,19 @@
 function main() {
 
+    var num1 = parseInt(prompt("Enter the first number"));
+    var num2 = parseInt(prompt("Enter the second number"));
+
+    alert("The product of the numbers is: " + product(num1, num2));
+    alert("The quotient of the numbers is: " + quotient(num1, num2));
+
+}
+function product(number1, number2){
+    var multiply = number1 * number2;
+    return multiply;
 }
 
+function quotient(number1, number2){
+    var divide = number1/number2;
+    return divide;
+}
 main();
